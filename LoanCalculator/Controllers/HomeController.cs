4c4dd018ace347555b33503d3ce308e122182b28
Kemyl <kemylfernandez299@gmail.com>
2023-09-15 16:36:28 -0400

    cASI

--- LoanCalculator/Controllers/HomeController.cs
+++ LoanCalculator/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using LoanCalculator.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 
 namespace LoanCalculator.Controllers
@@ -15,7 +16,33 @@ namespace LoanCalculator.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var model = new PrestamoViewModel
+            {
+                TiposPrestamo = new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Personal", Value = "Personal" },
+                    new SelectListItem { Text = "Automóvil", Value = "Automóvil" },
+                    new SelectListItem { Text = "Hipotecario", Value = "Hipotecario" }
+                },
+                Plazos = new List<SelectListItem>(),
+            };
+
+            // Agregar plazos desde 12 hasta 120 meses en incrementos de 6 meses.
+            for (int i = 12; i <= 120; i += 6)
+            {
+                model.Plazos.Add(new SelectListItem { Text = i.ToString(), Value = i.ToString() });
+            }
+
+            model.Tasa = 22;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult CalcularCuota(PrestamoViewModel model)
+        {
+            
+
+            return View("Index", model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ LoanCalculator/Models/PrestamoViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LoanCalculator.Models
+{
+    public class PrestamoViewModel
+    {
+        public decimal Monto { get; set; }
+        public string TipoPrestamo { get; set; }
+        public List<SelectListItem> TiposPrestamo { get; set; }
+        public int Plazo { get; set; }
+        public int Tasa { get; set; }
+        public List<SelectListItem> Plazos { get; set; }
+        public decimal? Resultado { get; set; }
+    }
+}
